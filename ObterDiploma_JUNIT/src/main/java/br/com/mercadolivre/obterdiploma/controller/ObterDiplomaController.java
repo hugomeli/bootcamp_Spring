1f1b81f534e6a4553df3e064ccdd15c16e52c271
huribeiro <hugo.ribeiro@mercadolivre.com>
2021-07-13 18:17:42 -0300

    Validações nos forms

--- ObterDiploma_JUNIT/src/main/java/br/com/mercadolivre/obterdiploma/controller/ObterDiplomaController.java
+++ ObterDiploma_JUNIT/src/main/java/br/com/mercadolivre/obterdiploma/controller/ObterDiplomaController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class ObterDiplomaController {
 
@@ -19,7 +21,7 @@ public class ObterDiplomaController {
     }
 
     @PostMapping("/analyzeNotes")
-    public ResponseDTO obterDiploma(@RequestBody StudentDTO studentDTO){
+    public ResponseDTO obterDiploma(@Valid @RequestBody StudentDTO studentDTO){
         return diplomaService.getDiploma(StudentDTO.converteDTOparaAluno(studentDTO));
     }
 }
--- ObterDiploma_JUNIT/src/main/java/br/com/mercadolivre/obterdiploma/model/forms/StudentDTO.java
+++ ObterDiploma_JUNIT/src/main/java/br/com/mercadolivre/obterdiploma/model/forms/StudentDTO.java
@@ -3,28 +3,39 @@ package br.com.mercadolivre.obterdiploma.model.forms;
 import br.com.mercadolivre.obterdiploma.model.entities.Student;
 import br.com.mercadolivre.obterdiploma.model.entities.Subject;
 
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public class StudentDTO {
 
-    @NotNull
+    @NotBlank
+    @Size(min = 8, max = 50)
+    @Pattern(regexp = "^[A-Za-z_ ]*$")
     private String name;
-    private List<Subject> subjects;
+
+    @Valid
+    @NotNull
+    @Size(max = 10)
+    private List<SubjectDTO> subjectsDTO;
 
 
     public StudentDTO() {
     }
 
-    public StudentDTO(String name, List<Subject> subjects) {
+    public StudentDTO(String name, List<SubjectDTO> subjectsDTO) {
         this.name = name;
-        this.subjects = subjects;
+        this.subjectsDTO = subjectsDTO;
     }
 
     public static Student converteDTOparaAluno(StudentDTO studentDTO){
         return new Student(
                 studentDTO.name,
-                studentDTO.subjects
+                SubjectDTO.convert(studentDTO.subjectsDTO)
         );
     }
 
@@ -36,11 +47,19 @@ public class StudentDTO {
         this.name = name;
     }
 
-    public List<Subject> getSubjects() {
-        return subjects;
+    public List<SubjectDTO> getSubjects() {
+        return subjectsDTO;
     }
 
-    public void setSubjects(List<Subject> subjects) {
-        this.subjects = subjects;
+    public void setSubjects(List<SubjectDTO> subjectsDTO) {
+        this.subjectsDTO = subjectsDTO;
+    }
+
+    public List<SubjectDTO> getSubjectsDTO() {
+        return subjectsDTO;
+    }
+
+    public void setSubjectsDTO(List<SubjectDTO> subjectsDTO) {
+        this.subjectsDTO = subjectsDTO;
     }
 }
